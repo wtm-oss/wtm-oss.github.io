0c214dabeb050dc45da380ac38edf491b0d64c40
Santosh Vijapure <vijapuresantosh@gmail.com>
2019-02-09 00:16:41 +0530

    new imgs added statistics added

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 50) {
+    $('.navbar').css('background','white');
+    } else {
+    $('.navbar').css('background','transparent','color');
+    $('')
+    }
+    });
